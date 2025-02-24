b889b38e01a4c1ca115b0e90f2b1f619c517cebc
Oren <xxorenxx@gmail.com>
2025-02-24 09:26:56 +0200

    added more options

--- server.js
+++ server.js
@@ -25,9 +25,12 @@ const corsOptions = {
   origin: [
     'http://127.0.0.1:3000',
     'http://localhost:3000',
+    'http://localhost:5173',
     'http://127.0.0.1:5173',
+    'http://127.0.0.1:5174',
     'http://localhost:5174',
     'http://localhost:5175',
+    'http://127.0.0.1:5175',
   ],
   credentials: true,
 }
